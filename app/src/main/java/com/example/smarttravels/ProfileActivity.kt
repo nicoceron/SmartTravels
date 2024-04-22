00774cb20d7ef2d16332e93a441b7b489a7815b2
WyzVy <sa-molina@javeriana.edu.co>
2024-04-22 17:01:13 -0500

    Inclusión del perfil.

---
+++ app/src/main/java/com/example/smarttravels/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.smarttravels
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile) // Reemplaza con el nombre de tu XML/layout
+        // Otro código de inicialización si es necesario
+    }
+}
